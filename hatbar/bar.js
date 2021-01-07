03a38654fa23fdd3fc4581ccc8c58bcc6e0ad0b1
Radek <radek@madenet.pl>
2021-01-07 10:44:19 +0100

    Code format

--- hatbar/bar.js
+++ hatbar/bar.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HatBar
 // @namespace    https://github.com/rikonek/job
-// @version      0.3
+// @version      0.4
 // @description  Work time counter
 // @author       Rikon
 // @match        https://hat.poland.[YOUR_DOMAIN_HERE].com/*
@@ -12,14 +12,13 @@
 GM_addStyle(`
 	#hatbar { position: fixed; z-index: 999; top: 0px; border-bottom: 1px solid #cccccc; background-color: yellow; width: 100%; padding: 7px; text-align: center; }
 	#hatbar_synchro { position: absolute; top: 5px; right: 20px; }
-	#hatbar_time_fix { background: #ffffff; border: 1px solid #aaaaaa; width: 150px; font-weight: normal; }
+	#hatbar_time_fix, .hatbar_form { background: #ffffff; border: 1px solid #aaaaaa; width: 150px; font-weight: normal; }
 `);
 
-function init()
-{
+function init() {
 	addBar();
 
-	$('#hatbar_time_fix').change(function() {
+	$('#hatbar_time_fix').change(function () {
 		setTimeFix($(this).val());
 		location.reload();
 	});
@@ -29,30 +28,25 @@ function init()
 	getWorkTime();
 }
 
-function set(key,value)
-{
-	localStorage.setItem('hatbar_'+key,value);
+function set(key, value) {
+	localStorage.setItem('hatbar_' + key, value);
 }
 
-function get(key)
-{
-	return localStorage.getItem('hatbar_'+key);
+function get(key) {
+	return localStorage.getItem('hatbar_' + key);
 }
 
-function addBar()
-{
+function addBar() {
 	$('body').prepend('<div id="hatbar">Czas pracy: <span id="hatbar_today">00:00:00</span>, Koniec: <span id="hatbar_end">00:00</span> (<span id="hatbar_remaining">00:00:00</span>)<br />Do wypracowania: <span id="hatbar_estimated">00:00:00</span>, Wypracowane: <span id="hatbar_sum">00:00:00</span><div id="hatbar_synchro"><input type="text" id="hatbar_time_fix" placeholder="Korekta czasu [minuty]" /></div></div>');
 }
 
-function getEvents(date)
-{
-	$.get("/activities/get-access-events/"+date,function(d) {
-		d=eval(d);
-		var tab={};
-		$.each(d,function(index,value) {
-			if(!tab[value.u])
-			{
-				tab[value.u]=[];
+function getEvents(date) {
+	$.get("/activities/get-access-events/" + date, function (d) {
+		d = eval(d);
+		var tab = {};
+		$.each(d, function (index, value) {
+			if (!tab[value.u]) {
+				tab[value.u] = [];
 			}
 			tab[value.u].push(value.det.tm);
 		});
@@ -61,136 +55,150 @@ function getEvents(date)
 	});
 }
 
-function readTimeFromEvents(tab)
-{
+function readTimeFromEvents(tab) {
 	$('#content').append("<br /><br />");
-	$.each(tab,function(data,arr) {
+	$.each(tab, function (data, arr) {
 		tab[data].sort();
-		var start=new Date(arr[0]);
-		var end=new Date(arr[arr.length-1]);
-		var d=start.toISOString().replace('-','').replace('-','').substr(0,8);
-		var now=new Date();
-		t=now.toISOString().replace('-','').replace('-','').substr(0,8);
-		if(d==t)
-		{
-			today_start=start;
-			end=now;
+		var start = new Date(arr[0]);
+		var end = new Date(arr[arr.length - 1]);
+		var d = start.toISOString().replace('-', '').replace('-', '').substr(0, 8);
+		var now = new Date();
+		t = now.toISOString().replace('-', '').replace('-', '').substr(0, 8);
+		if (d == t) {
+			today_start = start;
+			end = now;
 		}
 	});
 }
 
-function getWorkTime()
-{
-	var now=new Date();
-	now=now.toISOString().substr(0,10);
-	var time_div=$('div.s');
-	$.each(time_div,function(index,value) {
-		if($(this).attr('id')==now)
-		{
-			getEvents(now.substr(0,7));
-		}
-		else
-		{
-			month_estimated+=parseInt($(this).attr('norm'))*3600*1000;
-			month_sum+=parseInt($(this).attr('sum'))*1000;
+function getWorkTime() {
+	var now = new Date();
+	now = now.toISOString().substr(0, 10);
+	var time_div = $('div.s');
+	$.each(time_div, function (index, value) {
+		if ($(this).attr('id') == now) {
+			getEvents(now.substr(0, 7));
+		} else {
+			month_estimated += parseInt($(this).attr('norm')) * 3600 * 1000;
+			month_sum += parseInt($(this).attr('sum')) * 1000;
 		}
 	});
-	month_sum+=get('manual_time_fix')*60*1000;
+	month_sum += get('manual_time_fix') * 60 * 1000;
 	refreshConstTime();
+	vip();
 }
 
-function tFormat(t,sec=1)
-{
-	if(t<0)
-	{
-		t=t*(-1);
+function tFormat(t, sec = 1) {
+	if (t < 0) {
+		t = t * (-1);
 	}
-	t=t/1000;
-	h=parseInt(t/3600,0);
-	m=parseInt((t-(h*3600))/60);
-	s=parseInt(t-(h*3600)-(m*60));
-	if(h<10)
-	{
-		ti=("0"+h).slice(-2);
+	t = t / 1000;
+	h = parseInt(t / 3600, 0);
+	m = parseInt((t - (h * 3600)) / 60);
+	s = parseInt(t - (h * 3600) - (m * 60));
+	if (h < 10) {
+		ti = ("0" + h).slice(-2);
+	} else {
+		ti = h;
 	}
-	else
-	{
-		ti=h;
-	}
-	ti=ti+':'+("0"+m).slice(-2);
-	if(sec==1)
-	{
-		ti=ti+':'+("0"+s).slice(-2);
+	ti = ti + ':' + ("0" + m).slice(-2);
+	if (sec == 1) {
+		ti = ti + ':' + ("0" + s).slice(-2);
 	}
 	return ti;
 }
 
-function getTodayTime()
-{
-	now=new Date();
-	diff=now-today_start;
+function getTodayTime() {
+	now = new Date();
+	diff = now - today_start;
 	return diff;
 }
 
-function getEndTime()
-{
-	diff=month_sum-month_estimated;
-	start=(today_start.getHours()*3600+today_start.getMinutes()*60+today_start.getSeconds())*1000;
-	end=(8*3600*1000)+start-diff;
+function getEndTime() {
+	diff = month_sum - month_estimated;
+	start = (today_start.getHours() * 3600 + today_start.getMinutes() * 60 + today_start.getSeconds()) * 1000;
+	end = (8 * 3600 * 1000) + start - diff;
 	return end;
 }
 
-function getRemainingTime()
-{
-	currdate=new Date();
-	now=(currdate.getHours()*3600+currdate.getMinutes()*60+currdate.getSeconds())*1000;
-	remaining=getEndTime()-now;
-	if(remaining<0)
-	{
-		remaining=remaining*(-1);
-		remaining_sign='+';
+function getRemainingTime() {
+	currdate = new Date();
+	now = (currdate.getHours() * 3600 + currdate.getMinutes() * 60 + currdate.getSeconds()) * 1000;
+	remaining = getEndTime() - now;
+	if (remaining < 0) {
+		remaining = remaining * (-1);
+		remaining_sign = '+';
+	} else {
+		remaining_sign = '-';
 	}
-	else
-	{
-		remaining_sign='-';
-	}
-	return remaining_sign+tFormat(remaining);
+	return remaining_sign + tFormat(remaining);
 }
 
-function refreshConstTime()
-{
+function refreshConstTime() {
 	$('#hatbar_estimated').html(tFormat(month_estimated));
 	$('#hatbar_sum').html(tFormat(month_sum));
 }
 
-function refreshTime()
-{
-	if(!today_start) return false;
+function refreshTime() {
+	if (!today_start) {
+		return false;
+	}
 	$('#hatbar_today').html(tFormat(getTodayTime()));
 	$('#hatbar_remaining').html(getRemainingTime());
-	$('#hatbar_end').html(tFormat(getEndTime(),0));
+	$('#hatbar_end').html(tFormat(getEndTime(), 0));
 }
 
-function parseTime(str)
-{
-	var s=str.split(':');
-	return (s[0]*3600+s[1]*60+s[2]*1)*1000;
+function parseTime(str) {
+	var s = str.split(':');
+	return (s[0] * 3600 + s[1] * 60 + s[2] * 1) * 1000;
 }
 
-function setTimeFix(value)
-{
-	val=parseInt(value);
-	if(val==0 || !$.isNumeric(val)) val='';
-	set('manual_time_fix',val);
+function setTimeFix(value) {
+	val = parseInt(value);
+	if (val == 0 || !$.isNumeric(val)) {
+		val = '';
+	}
+	set('manual_time_fix', val);
+}
+
+function vip() {
+	if (get('vip')) {
+		$('#hatbar').append('<div style="position: absolute; top: 5px; left: 20px;"><input type="time" min="06:00" max="10:00" id="hatbar_start" class="hatbar_form" /></div>');
+		$('#hatbar_start').val(get('start'));
+		$('#hatbar_start').change(function () {
+			set('start', $(this).val());
+		});
+		if (get('start')) {
+			refresh = setInterval(function () { location.reload(); }, 300000); // 5 minutes
+			start_stop = setInterval(function () { vipCheck(); }, 20000); // 20 seconds
+		} else if (refresh) {
+			stopInterval(refresh);
+			stopInterval(start_stop);
+		}
+	}
+}
+
+function vipCheck() {
+	var start = parseTime(get('start') + ':00');
+	var stop = start + (8 * 3600 * 1000);
+	var now = parseTime(new Date().toLocaleTimeString('en-US', { hour12: false, hour: "numeric", minute: "numeric", second: "numeric" }));
+	if (start <= now && now >= (6 * 3600 * 1000) && now <= (10 * 3600 * 1000) && $('.sublnk.w span small').html() != 'I am working') {
+		document.location.href = '/activities/set-work/8';
+	}
+	if (stop <= now && $('.sublnk.w span small').html() == 'I am working') {
+		document.location.href = '/users/logout-finish';
+	}
 }
 
 var today_start;
-var month_estimated=0;
-var month_sum=0;
+var month_estimated = 0;
+var month_sum = 0;
+var refresh;
+var start_stop;
 
-(function() {
+(function () {
 	'use strict';
 
 	init();
-	setInterval(function() { refreshTime(); },1000);
+	setInterval(function () { refreshTime(); }, 1000);
 })();
